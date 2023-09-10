b2494c988e930aa393d0d0d7789e0b8e9b6f410b
GTPSHAX <143262216+GTPSHAX@users.noreply.github.com>
2023-09-11 03:45:03 +0700

    Update script.js

--- Portfolio V2/assets/script.js
+++ Portfolio V2/assets/script.js
@@ -19,6 +19,20 @@ function tes() {
         starContainer2.appendChild(star2);
     }
 }
-    tes()
+    for (let i = 1; i <= numberOfStars; i++) {
+        const star1 = document.createElement("div");
+        star1.className = "bi bintangs1";
+        star1.style.left = Math.random() * 1000 + "px";
+        star1.style.top = Math.random() * 1000 + "px";
+        // star1.style.zIndex = '1';
+        starContainer1.appendChild(star1);
+
+        const star2 = document.createElement("div");
+        star2.className = "bi bintangs2";
+        star2.style.left = Math.random() * 1000 + "px";
+        star2.style.top = Math.random() * 1000 + "px";
+        star2.style.zIndex = "0";
+        starContainer2.appendChild(star2);
+    }
     setInterval(tes, 10000);
 });
